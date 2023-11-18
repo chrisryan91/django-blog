99f1af2e72fe9117a6f9584f4c823986f7831f49
Chris <christopherryan91@gmail.com>
2023-11-18 15:58:20 +0000

    Allowed host in setting py

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-djang0.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-djang0-b589331cbb64.herokuapp.com', 'codestar-djang0.herokuapp.com', 'localhost']
 
 
 # Application definition
